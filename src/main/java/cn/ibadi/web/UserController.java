81a112b3123f07595b8e770fb51112ffef314c5c
jhon <jhonRawls@gmail.com>
2016-05-23 21:43:01 +0800

    第一次提交

---
+++ src/main/java/cn/ibadi/web/UserController.java
@@ -0,0 +1,32 @@
+package cn.ibadi.web;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	Logger logger=Logger.getLogger(UserController.class);
+	@RequestMapping(value = "login",method={RequestMethod.GET})
+	public String toLogin(ModelMap model) {
+		logger.debug("调用方法------");
+		 model.addAttribute("spring", "Hello Spring MVC Framework!");
+		 try {
+			int  a=0;
+			int b=12;
+			int c=b/a;
+		} catch (Exception e) {
+			logger.info("方法错误:{}",e);
+			logger.error("测试logj4j的打印地址");
+			// TODO: handle exception
+		}
+		return "login";
+	}
+	
+	
+
+}
