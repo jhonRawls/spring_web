37daa90e67a611146ec0a3227a53717ec4121aaf
zhouqian <zhouqian@12308.com>
2016-06-21 18:23:48 +0800

    response过滤特殊字符

--- src/main/java/cn/ibadi/utils/XssFilter.java
+++
@@ -1,8 +0,0 @@
-package cn.ibadi.utils;
-
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-
-public class XssFilter extends MappingJackson2HttpMessageConverter
-{
-
-}
