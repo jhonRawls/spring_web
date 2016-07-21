a331e0a2f82eb8ce1f431a8368cd1c8ad662c8ff
zhouqian <zhouqian@12308.com>
2016-07-21 10:08:42 +0800

    excel 导出功能

--- src/main/java/cn/ibadi/utils/export/ExcelServlet.java
+++ src/main/java/cn/ibadi/utils/export/ExcelServlet.java
@@ -1,27 +1,112 @@
 package cn.ibadi.utils.export;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import cn.ibadi.vo.Project;
+
 public class ExcelServlet extends HttpServlet {
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		(new ExportExcel()).test();
-		String str = "a.xls";
-		// String path =
-		// request.getSession().getServletContext().getRealPath(str);
-		download("E://a.xls", response);
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7147192553453823598L;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+		String fileName="excel文件";
+        //填充projects数据
+        List<Project> projects=createData();
+        List<Map<String,Object>> list=createExcelRecord(projects);
+        String columnNames[]={"ID","项目名","销售人","负责人","所用技术","备注"};//列名
+        String keys[]    =     {"id","name","saler","principal","technology","remarks"};//map中的key
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        try {
+            ExcelUtil.createWorkBook(list,keys,columnNames).write(os);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        byte[] content = os.toByteArray();
+        InputStream is = new ByteArrayInputStream(content);
+        // 设置response参数，可以打开下载页面
+        response.reset();
+        response.setContentType("application/vnd.ms-excel;charset=utf-8");
+        response.setHeader("Content-Disposition", "attachment;filename="+ new String((fileName + ".xls").getBytes(), "iso-8859-1"));
+        ServletOutputStream out = response.getOutputStream();
+        BufferedInputStream bis = null;
+        BufferedOutputStream bos = null;
+        try {
+            bis = new BufferedInputStream(is);
+            bos = new BufferedOutputStream(out);
+            byte[] buff = new byte[2048];
+            int bytesRead;
+            // Simple read/write loop.
+            while (-1 != (bytesRead = bis.read(buff, 0, buff.length))) {
+                bos.write(buff, 0, bytesRead);
+            }
+        } catch (final IOException e) {
+            throw e;
+        } finally {
+            if (bis != null)
+                bis.close();
+            if (bos != null)
+                bos.close();
+        }
+    }
+
+
+    private List<Project> createData() {
+        // TODO Auto-generated method stub
+        //自己实现
+    	 List<Project> projectList=new ArrayList<Project>();
+    	 for (int i = 0; i <15; i++) {
+    		 Project project=new Project();
+    		 project.setId(i);
+    		 project.setName("name : "+i);
+    		 project.setRemarks("remark : "+i);
+    		 project.setTechnology("technology : "+i);
+    		 projectList.add(project);
+		}
+        return projectList;
+    }
+    private List<Map<String, Object>> createExcelRecord(List<Project> projects) {
+        List<Map<String, Object>> listmap = new ArrayList<Map<String, Object>>();
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("sheetName", "sheet1");
+        listmap.add(map);
+        Project project=null;
+        for (int j = 0; j < projects.size(); j++) {
+            project=projects.get(j);
+            Map<String, Object> mapValue = new HashMap<String, Object>();
+            mapValue.put("id", project.getId());
+            mapValue.put("name", project.getName());
+            mapValue.put("technology", project.getTechnology());
+            mapValue.put("remarks", project.getRemarks());
+            listmap.add(mapValue);
+        }
+        return listmap;
+    }
 		
-	}
+		
+		
+		
+		
+		
+
 
 	private void download(String path, HttpServletResponse response) {
 		try {
